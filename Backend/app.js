956d022dc393c19147c6225e71328001daa504cd
kisun-sah <kishunsah783@gmail.com>
2025-02-06 10:19:27 +0530

    add sucess roue of project

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,22 @@
 import express from "express";
-import morgan from   "morgan";
+import morgan from "morgan";
 import connect from "./db/db.js";
 import userRoutes from "./routes/user.route.js";
+import projectRoutes from "./routes/project.routes.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 
 connect();
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/user' ,userRoutes);
+app.use("/user", userRoutes);
+app.use("/projects", projectRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World  !!");
 });
